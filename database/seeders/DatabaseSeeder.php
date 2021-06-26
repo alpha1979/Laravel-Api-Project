0fc205452ac318854d0c531d56cf846181358237
alpha1979 <atit1979@hotmail.com>
2021-06-26 23:28:40 +0100

    first initialise

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Petition;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        // Petition::factory(50)->create();
+        
+    }
+}
